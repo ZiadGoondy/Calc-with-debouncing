228e7503c3a03522154b3b9e6ce2a21ea8134c69
GooNDy <zyiadtarek@gmail.com>
2020-02-11 16:28:42 +0200

    mody

--- bitoperations.h
+++ bitoperations.h
@@ -1,6 +1,6 @@
 #ifndef BITOPERATIONS
 #define BITOPERATIONS
-#define SET_BIT(X , BIT) (X |= (unsigned char)(1<<BIT))
+#define SET_BIT(X , BIT) (X |= (unsigned char)(1<<BIT))//set bit >>1
 #define CLR_BIT(X , BIT) (X &=~(unsigned char)(1<<BIT))
 #define TOG_BIT(X , BIT) (X ^= (unsigned char)(1<<BIT))
 #define GET_BIT(X , BIT) ((X>>BIT) & (unsigned char) 1)
